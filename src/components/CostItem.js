9cc1db9d61e3def0f53a1c3d257d43a794480576
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-06 11:49:25 +0200

    09-Создание Компонента

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,13 @@
 function CostItem() {
-  return <h2>Элемент Расхода</h2>;
+  return (
+    <div>
+      <div>март 2021 12</div>
+      <div>
+        <h2>Холодильник</h2>
+        <div>$999.99</div>
+      </div>
+    </div>
+  );
 }
 
 export default CostItem;
