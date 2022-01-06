b2e2189c456e8aa0a2bd1aada5cd897fde7ab8ea
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-06 12:16:36 +0200

    11-Добавление  CSS Стилей

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,10 +1,12 @@
+import "./CostItem.css";
+
 function CostItem() {
   return (
-    <div>
+    <div className="cost-item">
       <div>март 2021 12</div>
-      <div>
+      <div className="cost-item__description">
         <h2>Холодильник</h2>
-        <div>$999.99</div>
+        <div className="cost-item__price">$999.99</div>
       </div>
     </div>
   );
