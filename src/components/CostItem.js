f873c376751eed42de0656fe378515a81f19f89b
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-07 13:36:56 +0200

    14-JavaScript Логика в Компонентах

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,9 +1,17 @@
 import "./CostItem.css";
 
 function CostItem(props) {
+  const month = props.date.toLocaleString("ru-RU", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("ru-RU", { day: "2-digit" });
+
   return (
     <div className="cost-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="cost-item__description">
         <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
