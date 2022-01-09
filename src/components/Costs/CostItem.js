3fe5ddb97c614641cef0baff90a1fe3013f162e6
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-09 15:41:29 +0200

    03-Как Выполняются Функции-Компоненты

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -3,15 +3,18 @@ import CostDate from "./CostDate";
 import Card from "../UI/Card";
 
 const CostItem = (props) => {
+  let description = props.description;
+
   const changeDescriptionHandler = () => {
-    console.log("Click!");
+    description = "New Text";
+    console.log(description);
   };
 
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
       <button onClick={changeDescriptionHandler}>Изменить Описание</button>
