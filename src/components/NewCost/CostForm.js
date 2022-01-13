539864fde57392f41257bebc1f69916d040eae39
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-13 17:00:31 +0200

    11-Обработка Отправки Формы

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import "./CostForm.css";
 
 const CostForm = () => {
-  const [name, setName] = useState("");
-  const [amount, setAmount] = useState("");
-  const [date, setDate] = useState("");
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
 
   //   const [userInput, setUserInput] = useState({
   //     name: "",
@@ -13,7 +13,7 @@ const CostForm = () => {
   //   });
 
   const nameChangeHandler = (event) => {
-    setName(event.target.value);
+    setInputName(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   name: event.target.value,
@@ -27,7 +27,7 @@ const CostForm = () => {
   };
 
   const amountChangeHandler = (event) => {
-    setAmount(event.target.value);
+    setInputAmount(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   amount: event.target.value,
@@ -35,15 +35,27 @@ const CostForm = () => {
   };
 
   const dateChangeHandler = (event) => {
-    setDate(event.target.value);
+    setInputDate(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   date: event.target.value,
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+
+    console.log(costData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-cost__controls">
         <div className="new-cost__control">
           <label>Название</label>
