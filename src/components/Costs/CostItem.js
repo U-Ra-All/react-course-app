b965ad422fd525a0bfc5f7ba4a0e946f336eea23
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-14 14:59:29 +0200

    17-Компоненты - Контролируемые vs Неконтролируемые, с Состоянием vs без Состояния

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,26 +1,15 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
 
 const CostItem = (props) => {
-  const [description, setDescription] = useState(props.description);
-  console.log("Text in component definition");
-
-  const changeDescriptionHandler = () => {
-    // description = 'New cost';
-    setDescription("New cost"); // description = 'New cost'; & CostItem(props);
-    console.log(description);
-  };
-
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
-      <button onClick={changeDescriptionHandler}>Изменить Описание</button>
     </Card>
   );
 };
