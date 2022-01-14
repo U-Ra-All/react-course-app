c17b4bb3b4e123244a275382149024a59cf1165f
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-14 13:34:15 +0200

    16-Задание 2. Решение

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,26 +1,37 @@
 import CostItem from "./CostItem";
 import "./Costs.css";
 import Card from "../UI/Card";
+import CostsFilter from "./CostsFilter";
+import React, { useState } from "react";
 
 const Costs = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="costs">
-      <CostItem
-        date={props.costs[0].date}
-        description={props.costs[0].description}
-        amount={props.costs[0].amount}
-      />
-      <CostItem
-        date={props.costs[1].date}
-        description={props.costs[1].description}
-        amount={props.costs[1].amount}
-      />
-      <CostItem
-        date={props.costs[2].date}
-        description={props.costs[2].description}
-        amount={props.costs[2].amount}
-      />
-    </Card>
+    <div>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostItem
+          date={props.costs[0].date}
+          description={props.costs[0].description}
+          amount={props.costs[0].amount}
+        />
+        <CostItem
+          date={props.costs[1].date}
+          description={props.costs[1].description}
+          amount={props.costs[1].amount}
+        />
+        <CostItem
+          date={props.costs[2].date}
+          description={props.costs[2].description}
+          amount={props.costs[2].amount}
+        />
+      </Card>
+    </div>
   );
 };
 
---
+++ src/components/Costs/CostsFilter.js
@@ -0,0 +1,23 @@
+import "./CostsFilter.css";
+
+const CostsFilter = (props) => {
+  const yearChangeHandler = (event) => {
+    props.onChangeYear(event.target.value);
+  };
+
+  return (
+    <div className="costs-filter">
+      <div className="costs-filter__control">
+        <label>Выбор По Году</label>
+        <select value={props.year} onChange={yearChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default CostsFilter;
