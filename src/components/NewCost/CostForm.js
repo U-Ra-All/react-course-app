f43c1959e081380a35094d88fe9d08b22b5fc90b
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-18 15:08:26 +0200

    Задание 4. Решение

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -86,6 +86,9 @@ const CostForm = (props) => {
         </div>
         <div className="new-cost__actions">
           <button type="submit">Добавить Расход</button>
+          <button type="button" onClick={props.onCancel}>
+            Отмена
+          </button>
         </div>
       </div>
     </form>
--- src/components/NewCost/NewCost.js
+++ src/components/NewCost/NewCost.js
@@ -1,7 +1,10 @@
+import React, { useState } from "react";
 import CostForm from "./CostForm";
 import "./NewCost.css";
 
 const NewCost = (props) => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   const saveCostDataHandler = (inputCostData) => {
     const costData = {
       ...inputCostData,
@@ -9,11 +12,28 @@ const NewCost = (props) => {
     };
 
     props.onAddCost(costData);
+    setIsFormVisible(false);
+  };
+
+  const inputCostDataHandler = () => {
+    setIsFormVisible(true);
+  };
+
+  const cancelCostHandler = () => {
+    setIsFormVisible(false);
   };
 
   return (
     <div className="new-cost">
-      <CostForm onSaveCostData={saveCostDataHandler} />
+      {!isFormVisible && (
+        <button onClick={inputCostDataHandler}>Добавить Новый Расход</button>
+      )}
+      {isFormVisible && (
+        <CostForm
+          onSaveCostData={saveCostDataHandler}
+          onCancel={cancelCostHandler}
+        />
+      )}
     </div>
   );
 };
