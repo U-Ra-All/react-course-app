6447037d3497e99d121b7ffbe181787f506d3890
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-18 16:35:54 +0200

    12-Динамические Стили

--- src/components/Diagram/DiagramBar.js
+++ src/components/Diagram/DiagramBar.js
@@ -1,3 +1,25 @@
-const DiagramBar = () => {};
+import "./DiagramBar.css";
+
+const DiagramBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="diagram-bar">
+      <div className="diagram-bar__inner">
+        <div
+          className="diagram-bar__fill"
+          style={{
+            height: barFillHeight,
+          }}
+        ></div>
+      </div>
+      <div className="diagram-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
 export default DiagramBar;
